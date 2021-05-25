7ed324fd29048cce23e0fd7c35eb4a70609bc026
ctltlima <ctl_tlima@uolinc.com>
2021-05-25 07:44:12 -0300

    ajustando a cobertura dos testes

--- src/main/java/br/com/escola/dtos/AlunoDTO.java
+++ src/main/java/br/com/escola/dtos/AlunoDTO.java
@@ -1,7 +1,5 @@
 package br.com.escola.dtos;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,8 +7,6 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class AlunoDTO {
 
     private Long id;
